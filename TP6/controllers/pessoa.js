6389c1fda4a8d32100d65e181513a788612b91ca
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2023-03-26 19:22:46 +0100

    add refactor_body function
    - Delivers an object with the format to be inserted in the db

--- TP6/controllers/pessoa.js
+++ TP6/controllers/pessoa.js
@@ -2,6 +2,63 @@ var axios = require('axios')
 var pessoa = require('../models/pessoa')
 var pessoasModel =  pessoa.pessoasModel
 
+var assign_bool = (value) => {
+   if (!value) {
+        return value
+   } else {
+        return value === 'true' 
+   }
+
+}
+var refactor_body = (a) => {
+     var morada = {
+        cidade: a.cidade,
+        distrito: a.distrito
+    }
+    
+    delete a.cidade
+    delete a.distrito
+    
+    var partido = {
+        party_abbr: a.party_abbr,
+        party_name: a.party_name,
+    }
+    
+    delete a.party_abbr
+    delete a.party_name
+
+    var atributos = {
+        fumador:                assign_bool(a.fumador                ),
+        gosta_cinema:           assign_bool(a.gosta_cinema           ),
+        gosta_viajar:           assign_bool(a.gosta_viajar           ), 
+        acorda_cedo:            assign_bool(a.acorda_cedo            ),
+        gosta_ler:              assign_bool(a.gosta_ler              ), 
+        gosta_musica:           assign_bool(a.gosta_musica           ),
+        gosta_comer:            assign_bool(a.gosta_comer            ), 
+        gosta_animais_estimacao:assign_bool(a.gosta_animais_estimacao),
+        gosta_dancar:           assign_bool(a.gosta_dancar           ),
+        comida_favorita: a.comida_favorita
+    }
+    
+    delete a.fumador
+    delete a.gosta_cinema
+    delete a.gosta_viajar
+    delete a.acorda_cedo
+    delete a.gosta_ler
+    delete a.gosta_musica
+    delete a.gosta_comer
+    delete a.gosta_animais_estimacao
+    delete a.gosta_dancar
+    delete a.comida_favorita
+    
+    a["morada"] = morada
+    a["partido_politico"] = partido
+    a["atributos"] = atributos
+    
+    console.log(a)
+
+    return a
+}
 // Student list
 module.exports.list = () => {
     return pessoasModel.find()
@@ -26,52 +83,7 @@ module.exports.getPessoa = id => {
 }
 
 module.exports.addPessoa = a => {
-    var morada = {
-        cidade: a.cidade,
-        distrito: a.distrito
-    }
-    
-    delete a.cidade
-    delete a.distrito
-    
-    var partido = {
-        party_abbr: a.party_abbr,
-        party_name: a.party_name,
-    }
-    
-    delete a.party_abbr
-    delete a.party_name
-
-    var atributos = {
-        fumador:      (a.fumador === 'true'),
-        gosta_cinema: (a.gosta_cinema === 'true'),
-        gosta_viajar: (a.gosta_viajar === 'true'), 
-        acorda_cedo:  (a.acorda_cedo ===  'true'),
-        gosta_ler:    (a.gosta_ler   ===  'true'), 
-        gosta_musica: (a.gosta_musica === 'true'),
-        gosta_comer:  (a.gosta_comer ===  'true'), 
-        gosta_animais_estimacao: (a.gosta_animais_estimacao === 'true'),
-        gosta_dancar: (a.gosta_dancar === 'true'),
-        comida_favorita: a.comida_favorita
-    }
-    
-    delete a.fumador
-    delete a.gosta_cinema
-    delete a.gosta_viajar
-    delete a.acorda_cedo
-    delete a.gosta_ler
-    delete a.gosta_musica
-    delete a.gosta_comer
-    delete a.gosta_animais_estimacao
-    delete a.gosta_dancar
-    delete a.comida_favorita
-    
-    a["morada"] = morada
-    a["partido_politico"] = partido
-    a["atributos"] = atributos
-
-    console.log(a)
-    return pessoasModel.create(a)
+    return pessoasModel.create(refactor_body(a))
             .then( pessoa => {
                 return pessoa
             })
@@ -81,7 +93,7 @@ module.exports.addPessoa = a => {
 }
 
 module.exports.updatePessoa = a => {
-    return pessoasModel.updateOne({id:a.id}, a)
+    return pessoasModel.updateOne({id:a.id}, refactor_body(a))
             .then( pessoa => {
                 return pessoasModel.findOne({id:a.id}).then(resp => {return resp})
             })
